6203cc127b753e31b6cd21df8baf00f6c9af328d
Greg Lapinski <grlap@microsoft.com>
2023-02-28 17:19:46 -0800

    install Helm, Kubectl, Minikube

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+# Docker
+
 sudo apt update -y
 
 sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
@@ -23,3 +25,30 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo service docker start
+
+# Helm
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+# Kubectl
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+
+sudo apt-get install -y apt-transport-https
+
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Minikube
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+
